54d33e3e71201f95b00e0817037bcc9f88fef267
Md Rafiqul Islam <rafiq@Mds-iMac.local>
2019-07-29 21:13:52 -0700

    Rating star

--- FoodTracker/RatingController.swift
+++ FoodTracker/RatingController.swift
@@ -17,7 +17,11 @@ import UIKit
         }
     }
     
-    var rating = 0
+    var rating = 0 {
+        didSet {
+            updateButtonSelectionStates()
+        }
+    }
     
     //MARK: Initialization
     override init(frame: CGRect) {
@@ -33,7 +37,21 @@ import UIKit
     //MARK: Button Action
     @objc
     func ratingButtonTapped(button: UIButton) {
-        print("Button pressed 👍")
+        
+        guard let index = ratingButtons.firstIndex(of: button) else {
+            fatalError("The button, \(button), is not in the ratingButtons array: \(ratingButtons)")
+        }
+        
+        // Calculate the rating of the selected button
+        let selectedRating = index + 1
+        
+        if selectedRating == rating {
+            // If the selected star represents the current rating, reset the rating to 0.
+            rating = 0
+        } else {
+            // Otherwise set the rating to the selected star
+            rating = selectedRating
+        }
     }
     
     //MARK: Private Methods
@@ -67,6 +85,9 @@ import UIKit
             button.heightAnchor.constraint(equalToConstant: starSize.height).isActive = true
             button.widthAnchor.constraint(equalToConstant: starSize.width).isActive = true
             
+            // Set the accessibility label
+            //button.accessibilityLabel = "Set \(rating) star rating"
+            
             // Setup the button action
             button.addTarget(self, action:
                 #selector(RatingController.ratingButtonTapped(button:)), for: .touchUpInside)
@@ -78,5 +99,15 @@ import UIKit
             ratingButtons.append(button)
         }
         
+        updateButtonSelectionStates()
+        
+    }
+    
+    private func updateButtonSelectionStates() {
+        for (index, button) in ratingButtons.enumerated() {
+            // If the index of a button is less than the rating, that button should be selected.
+            button.isSelected = index < rating
+            
+        }
     }
 }
